ffd82f0bcdacf215fd66fd77415da2c341e7e76f
Nathan Anderson <nathandanielanderson@gmail.com>
2021-02-06 16:39:18 -0600

    set up react app and piped movie data to cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Card = ({ movie }) => {
+  return (
+    <div className="card">
+      <img
+        src={'src/' + movie.img.src}
+        className="card-img-top"
+        alt={'src/' + movie.img.alt}
+      />
+      <div className="card-body">
+        <h2 className="card-title">{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">{`Distributor: ${movie.distributor}`}</li>
+        <li className="list-group-item">{`Amount: ${movie.amount}`}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Card;
